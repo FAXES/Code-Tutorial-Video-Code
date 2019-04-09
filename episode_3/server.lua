82625cb5b0ccb65fa92e61c240128a0df9cca0d2
Big-Yoda <43821187+Big-Yoda@users.noreply.github.com>
2019-04-09 21:27:23 +0200

    Update server.lua

--- episode_3/server.lua
+++ episode_3/server.lua
@@ -1,17 +1,12 @@
-
-
 -- /kick ID Reason
 RegisterCommand("kick", function(source, args, rawCommand)
-    local badBoi = tonumber(args[1])
-    local kickReason = table.concat(args, " ", 2)
+    local badBoi = tonumber(args[1]) or 0
+    local kickReason = table.concat(args, " ", 2) or "No Reason Provided"
 
     if GetPlayerName(source) == "FAXES" then
-        if badBoi then
-            if kickReason == nil then
-                kickReason = "No Reason Provided"
-            end
+        if GetPlayerName(badBoi) then
             -- Swapped these two around to stop any potential errors. Chat message should be before the drop.
-            TriggerClientEvent("chatMessage", -1, "^4^*[Player Kicked] ^7" .. GetPlayerName(badBoi) .. " was kicked for:" .. kickReason)
+            TriggerClientEvent("chatMessage", -1, "^4^*[Player Kicked] ^7" .. GetPlayerName(badBoi) .. " was kicked for: " .. kickReason)
             DropPlayer(badBoi, kickReason)
         else
             TriggerClientEvent("chatMessage", source, "^1Invalid Player. Usage /kick ID Reason") -- In the video it's -1 which is everyone, so lets make it 'source'
